5018cdeb0d55f00d544ca95d7e920417f5e9c067
Daniel Hammond <danielrhammond@gmail.com>
2016-03-02 12:04:09 -0800

    Add shorcut method for creating "auto-reversed" GIFs

--- GIFPlayground.playground/Sources/GIF.swift
+++ GIFPlayground.playground/Sources/GIF.swift
@@ -33,6 +33,7 @@ public struct Animation {
 
 extension Animation {
     public typealias FrameRenderer = (Int, CGContext) -> Void
+    /// Creates an animated GIF with the specified frame count, duration, size and a function to draw a frame given an index and a CGContext
     public static func create(frameCount: Int, width: Int, height: Int, frameDelay: NSTimeInterval, renderer: FrameRenderer) throws -> Animation {
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGImageAlphaInfo.PremultipliedLast.rawValue
@@ -50,6 +51,13 @@ extension Animation {
             frames.append(frameImage)
         }
         return Animation(frames: frames, frameDelay: frameDelay)
+    }
+    
+    /// Shortcut for creating a looping GIF where the second half reverses the frames of the first half so it ends where it begins
+    public static func createAutoReversedLoop(halfFrameCount: Int, width: Int, height: Int, frameDelay: NSTimeInterval, renderer: FrameRenderer) throws -> Animation {
+        let firstHalf = try create(halfFrameCount, width: width, height: height, frameDelay: frameDelay, renderer: renderer)
+        let secondHalf = firstHalf.frames.reverse().dropFirst().dropLast()
+        return Animation(frames: firstHalf.frames + secondHalf, frameDelay: frameDelay)
     }
 }
 
